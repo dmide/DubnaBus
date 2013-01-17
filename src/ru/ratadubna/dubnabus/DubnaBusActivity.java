eddd2f141e179a02c76d10b9dad1daf849d88c71
unknown <Dmide@Dmide.(none)>
2013-01-18 01:25:28 +0300

    async loading of routes maps; drawing these routes with random colors

--- src/ru/ratadubna/dubnabus/DubnaBusActivity.java
+++ src/ru/ratadubna/dubnabus/DubnaBusActivity.java
@@ -1,15 +1,30 @@
 package ru.ratadubna.dubnabus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class DubnaBusActivity extends SherlockFragmentActivity {
 	private GoogleMap mMap;
@@ -21,6 +36,11 @@ public class DubnaBusActivity extends SherlockFragmentActivity {
 	public void onResume() {
 		super.onResume();
 		setUpMapIfNeeded();
+		if (getIntent().hasExtra("idList")) {
+			ArrayList<Integer> idArray = getIntent().getIntegerArrayListExtra(
+					"idList");
+			DrawRoutes(idArray);
+		}
 	}
 
 	@Override
@@ -72,4 +92,87 @@ public class DubnaBusActivity extends SherlockFragmentActivity {
 		this.prefs = prefs;
 	}
 
+	void DrawRoutes(ArrayList<Integer> idArray) {
+		for (Integer id : idArray) {
+			GetRouteMapTask getRouteMapTask = new GetRouteMapTask(id);
+			ModelFragment.executeAsyncTask(getRouteMapTask, this
+					.getApplicationContext());
+		}
+	}
+
+	private class GetRouteMapTask extends AsyncTask<Context, Void, Void> {
+		private Exception e = null;
+		private int id;
+		private PolylineOptions mapRoute;
+
+		public GetRouteMapTask(int id) {
+			this.id = id;
+		}
+
+		@Override
+		protected Void doInBackground(Context... ctxt) {
+			BufferedReader reader = null;
+			try {
+				URL url = new URL("http://ratadubna.ru/nav/d.php?o=2&m="
+						+ String.valueOf(id));
+				HttpURLConnection c = (HttpURLConnection) url.openConnection();
+				c.setRequestMethod("GET");
+				c.setReadTimeout(15000);
+				c.connect();
+				reader = new BufferedReader(new InputStreamReader(
+						c.getInputStream()));
+				StringBuilder buf = new StringBuilder();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					buf.append(line + "\n");
+				}
+				mapRoute = ParseMapRoute(buf.toString());
+				if (mapRoute == null)
+					throw new Exception();
+			} catch (Exception e) {
+				Log.e(getClass().getSimpleName(),
+						"Exception retrieving bus routes content", e);
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						Log.e(getClass().getSimpleName(),
+								"Exception closing HUC reader", e);
+					}
+				}
+			}
+			return (null);
+		}
+
+		@Override
+		public void onPostExecute(Void arg0) {
+			if (e == null) {
+				mapRoute.color(-(new Random().nextInt(2147483647)));
+				mMap.addPolyline(mapRoute);
+			} else {
+				Log.e(getClass().getSimpleName(), "Exception loading contents",
+						e);
+			}
+		}
+	}
+
+	private PolylineOptions ParseMapRoute(String page) {
+		page = page.replaceAll(",", ".");
+		Pattern pattern = Pattern.compile("([0-9]{2}.[0-9]+)"), pattern2 = Pattern
+				.compile("route-menu-item([0-9]+)");
+		Matcher matcher = pattern.matcher(page);
+		PolylineOptions mapRoute = new PolylineOptions();
+		double lat, lng;
+		while (matcher.find()) {
+			lat = Double.parseDouble(matcher.group());
+			if (matcher.find())
+				lng = Double.parseDouble(matcher.group());
+			else
+				return null;
+			mapRoute.add(new LatLng(lat, lng));
+		}
+		matcher = pattern.matcher(page);
+		return mapRoute;
+	}
 }
--- src/ru/ratadubna/dubnabus/MenuFragment.java
+++ src/ru/ratadubna/dubnabus/MenuFragment.java
@@ -1,20 +1,9 @@
 package ru.ratadubna.dubnabus;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,16 +13,19 @@ import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MenuFragment extends SherlockFragment implements
 		android.widget.AdapterView.OnItemClickListener,
 		android.widget.AdapterView.OnClickListener {
 	private ListView lv;
-	private boolean MapRouteLoaded = false;
 	private Button but;
-	private ArrayList<Integer> idArray;
+	private ArrayList<Integer> idArray = new ArrayList<Integer>();
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setRetainInstance(true);
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,103 +49,19 @@ public class MenuFragment extends SherlockFragment implements
 	}
 
 	public void toggle(CheckedTextView v) {
-		if (v.isChecked()) {
-			v.setChecked(false);
-		} else {
-			v.setChecked(true);
-		}
+		v.setChecked(!v.isChecked());
 	}
 
 	public void onClick(View v) {
 		SparseBooleanArray sba = lv.getCheckedItemPositions();
 		for (int i = 0; i < sba.size(); i++) {
-			if (sba.get(i)) {
-				idArray.add(BusRoutes.GetRoutes().get(i).GetId());
-				GetRouteMapTask getRouteMapTask = new GetRouteMapTask(BusRoutes
-						.GetRoutes().get(i).GetId());
-				ModelFragment.executeAsyncTask(getRouteMapTask, getActivity()
-						.getApplicationContext());
+			if (sba.valueAt(i)) {
+				idArray.add(BusRoutes.GetRoutes().get(sba.keyAt(i)).GetId());
 			}
-
 		}
 		Intent i = new Intent(getActivity(), DubnaBusActivity.class);
 		i.putExtra("idList", idArray);
 		startActivity(i);
-	}
-
-	private class GetRouteMapTask extends AsyncTask<Context, Void, Void> {
-		private Exception e = null;
-		private int id;
-
-		public GetRouteMapTask(int id) {
-			this.id = id;
-		}
-
-		@Override
-		protected Void doInBackground(Context... ctxt) {
-			BufferedReader reader = null;
-			try {
-				URL url = new URL("http://ratadubna.ru/nav/d.php?o=2&m="
-						+ String.valueOf(id));
-				HttpURLConnection c = (HttpURLConnection) url.openConnection();
-				c.setRequestMethod("GET");
-				c.setReadTimeout(15000);
-				c.connect();
-				reader = new BufferedReader(new InputStreamReader(
-						c.getInputStream()));
-				StringBuilder buf = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					buf.append(line + "\n");
-				}
-				ParseMapRoute(buf.toString());
-				if (!MapRouteLoaded)
-					throw new Exception();
-			} catch (Exception e) {
-				Log.e(getClass().getSimpleName(),
-						"Exception retrieving bus routes content", e);
-			} finally {
-				if (reader != null) {
-					try {
-						reader.close();
-					} catch (IOException e) {
-						Log.e(getClass().getSimpleName(),
-								"Exception closing HUC reader", e);
-					}
-				}
-			}
-			return (null);
-		}
-
-		@Override
-		public void onPostExecute(Void arg0) {
-			if (e == null) {
-
-			} else {
-				Log.e(getClass().getSimpleName(), "Exception loading contents",
-						e);
-			}
-		}
-	}
-
-	private void ParseMapRoute(String page) {
-		Pattern pattern = Pattern.compile("([0-9]{2}.[0-9]+)"), pattern2 = Pattern
-				.compile("route-menu-item([0-9]+)");
-		Matcher matcher = pattern.matcher(page);
-		PolylineOptions mapRoute = new PolylineOptions();
-		int lat, lng;
-		while (matcher.find()) {
-			lat = Integer.parseInt(matcher.group());
-			if (matcher.find())
-				lng = Integer.parseInt(matcher.group());
-			else
-				return;
-			mapRoute.add(new LatLng(lat, lng));
-		}
-		mapRoute.color(Color.RED);
-		matcher = pattern.matcher(page);
-
-		MapRouteLoaded = true;
 	}
 
 }
